02b7fd738c6c477b254a52ffbdc7504ba2d559c6
Alina Kovtunovich <alinakovtunovich@yandex.ru>
2024-05-23 17:44:02 +0300

    Make the model identifiable

--- Today/Today/ListViewController/ReminderListViewController.swift
+++ Today/Today/ListViewController/ReminderListViewController.swift
@@ -7,31 +7,45 @@
 
 import UIKit
 
+
+// Класс ReminderListViewController наследуется от UICollectionViewController,
+// что позволяет использовать коллекцию для отображения данных и управления пользовательским интерфейсом
 class ReminderListViewController: UICollectionViewController {
     
+    //Свойство dataSource используется для управления данными в коллекции
     var dataSource: DataSource!
+    // reminders содержит массив объектов Reminder, которые будут отображаться в коллекции.
+    var reminders: [Reminder] = Reminder.sampleData
     
+    // Метод viewDidLoad() вызывается при загрузке экрана и используется для инициализации
+    // элементов и настройки пользовательского интерфейса.
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //  Объект listLayout() инициализирует и настраивает композиционный макет для отображения элементов в коллекции.
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
         
+        //cellRegistration используется для регистрации и настройки ячеек коллекции.
         let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         
+        // замыкание в инициализации определяет способ конфигурации ячейки.
         dataSource = DataSource(collectionView: collectionView) {
-            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Reminder.ID) in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
         
+        //Snapshot используется для создания снимка данных, который затем применяется к dataSource для отображения информации в коллекции
         var snapshot = Snapshot()
         snapshot.appendSections([0])
-        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+        snapshot.appendItems(reminders.map { $0.id })
         dataSource.apply(snapshot)
         
+        //collectionView.dataSource присваивается dataSource для определения источника данных коллекции
         collectionView.dataSource = dataSource
     }
 
+    // Приватный метод listLayout() используется для создания и настройки композиционного макета коллекции
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
         listConfiguration.showsSeparators = false
